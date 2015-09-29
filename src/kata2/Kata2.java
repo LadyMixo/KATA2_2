cf86575ad450b1d3d14062132663b2b6b6f19cc1
usuario <usuario@10.22.143.32>
2015-09-29 10:29:37 +0100

    Tercera Versión. Key genérico, Value genérico

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,25 +1,19 @@
-
 package kata2;
 
 import java.util.HashMap;
-
 
 public class Kata2 {
 
     public static void main(String[] args) {
 
-         // Integer[] vector = {1, 1, 3, 4, 5, 6, 8, 9, 100,7, 7, -4};
-        String [] vector = {"Ana", "Juan", "Pedro", "Lucia","Ana", "Juan", "Pedro", "Lucia"};
+        // Integer[] vector = {1, 1, 3, 4, 5, 6, 8, 9, 100,7, 7, -4};
+        String[] vector = {"Ana", "Juan", "Pedro", "Lucia", "Ana", "Juan", "Pedro", "Lucia"};
 
-        Histogram mihisto = new Histogram(vector);
-
-            
-        HashMap <String,Integer> histogram= mihisto.getHisto();
-    
+        Histogram <String> histogram = CalculaHisto.computeHistogram(vector);
         
-        for (String key: histogram.keySet()){
-            System.out.println(key + "-->"+histogram.get(key));
+
+        for (String key : histogram.keySet()) {
+            System.out.println(key + "-->" + histogram.get(key));
         }
     }
 }
-
